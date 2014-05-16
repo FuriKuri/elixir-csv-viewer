7ead553b686e20fcbab2c2820f8cdec335f00f5f
Theo Pack <tf.pack@gmail.com>
2014-05-17 00:37:33 +0200

    remove unused comments

--- lib/csv_viewer/supervisor.ex
+++ lib/csv_viewer/supervisor.ex
@@ -6,13 +6,7 @@ defmodule CsvViewer.Supervisor do
   end
 
   def init([]) do
-    children = [
-      # Define workers and child supervisors to be supervised
-      # worker(CsvViewer.Worker, [arg1, arg2, arg3])
-    ]
-
-    # See http://elixir-lang.org/docs/stable/Supervisor.Behaviour.html
-    # for other strategies and supported options
+    children = [ ]
     supervise(children, strategy: :one_for_one)
   end
 end
